5f7a3aa10690b5fbfacdba8b951dca716f4fa327
FlashScript <itzflashscript@gmail.com>
2024-07-12 11:07:44 +0330

    file & folder strucure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-
+import { Route, Routes } from "react-router-dom"
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed, NotFound } from './components';
 const App = () => {
 
     return (
-        <div>App</div>
+        <div className="bg-black text-white">
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetail />} />
+                <Route path="/channel/:id" element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
     )
 
 }
---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import VideoDetail from "./VideoDetail";
+import ChannelDetail from "./ChannelDetail";
+import SearchFeed from "./SearchFeed";
+import NotFound from "./NotFound";
+
+export {
+    Navbar,
+    Feed,
+    VideoDetail,
+    ChannelDetail,
+    SearchFeed,
+    NotFound
+}
