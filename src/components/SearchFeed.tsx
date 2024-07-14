793ac6e55069dcd51a0cf987587413a5addbfb91
FlashScript <itzflashscript@gmail.com>
2024-07-14 12:18:42 +0330

    Adding SearchTerm Component

--- src/components/SearchFeed.tsx
+++ src/components/SearchFeed.tsx
@@ -1,58 +1,35 @@
+import { useState } from "react"
+import Videos from "./Videos"
+import useYouTube from "../api/useYouTube";
+import { searchUserLink } from "../constants";
+import { setThisDataType } from "../types";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
 
-    return (
-        <div>SearchFeed</div>
-    )
+    // const [userInput, setUserInput] = useState("")
+    // const [userInputRestul, setUserInputRestul] = useState(null)
 
-}
-
-export default SearchFeed
-
-
-/*
-import { useState } from "react"
-import Sidebar from "./Sidebar"
-import Videos from "./Videos"
-import useYouTube from "../api/useYouTube";
-import { searchLink } from "../constants";
-import { setThisDataType } from "../types";
-
-const Feed = () => {
-
-    const [selectedCategory, setSelectedCategory] = useState("VS Code");
     const [mainData, setMainData] = useState<setThisDataType[] | null>([])
+
+    const { searchTerm } = useParams();
     
-    const { isLoading } = useYouTube(`${searchLink}${selectedCategory}`, setMainData)
+    const { isLoading } = useYouTube(`${searchUserLink}${searchTerm}`, setMainData)
 
-    // mainData && isFetched && console.log(mainData)
     return (
-        <main className="flex flex-col md:flex-row">
-            <aside className="h-auto md:h-[92vh] border-r border-r-[#3d3d3d] px-0 md:px-8">
-                <Sidebar 
-                    selectedCategory={selectedCategory} 
-                    setSelectedCategory={setSelectedCategory}
-                />
+        <main className="p-4 overflow-y-auto h-[90vh] flex-1">
+            <h1 className="text-4xl font-bold mb-4 text-white">
+                Search Result for: {" "} 
+                <span className="text-[#FC1503]">
+                    {searchTerm} 
+                </span>
+                {" "} vidoes
+            </h1>
 
-                <h5 className="copyright mt-6 text-white">
-                    Copyright 2024 FlashScript
-                </h5>
-            </aside>
-
-            <section className="p-4 overflow-y-auto h-[90vh] flex-1">
-                <h1 className="text-4xl font-bold mb-4 text-white">
-                    {selectedCategory} {" "}
-                    <span className="text-[#FC1503]">
-                        vidoes
-                    </span>
-                </h1>
-
-                {!isLoading && <Videos videos={mainData} />}
-            </section>
+            {!isLoading && <Videos videos={mainData} />}
         </main>
     )
 
 }
 
-export default Feed
-*/
+export default SearchFeed
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -56,6 +56,9 @@ export {
 
 export const searchLink: string = "search?part=snippet&q=learn%20";
 
+
 export const channelLink: string = "channels?part=snippet&id=";
 
 export const searchChannelLink: string = "search?part=snippet&order=date&channelId=";
+
+export const searchUserLink: string = "search?part=snippet&q=";
