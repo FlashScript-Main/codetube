b0dc09637f08a218fb71702fcd807ce024db3de3
FlashScript <itzflashscript@gmail.com>
2024-07-16 19:11:06 +0330

    Adding a simple Not found page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom"
+import { House, MoveDown, MoveLeft, MoveUp } from "lucide-react"
 
 const NotFound = () => {
 
     return (
-        <div>NotFound</div>
+        <div className="w-full h-screen grid place-content-center space-y-8 md:space-y-12 xl:space-y-20">
+            <h1 className="text-4xl md:text-7xl xl:text-9xl text-main-title font-semibold">
+                Are you Lost?
+            </h1>
+
+            <div className="grid place-content-center">
+                <h5 className="text-base flex md:text-3xl text-indigo-300 font-medium relative">
+                    Go back
+                    <Link to="/" className="flex justify-center items-center group">
+                        <span className="text-purple-400 font-bold ml-1 mr-1 group-hover:text-main-title">
+                            Home
+                        </span>
+                        <span>
+                            <House className="w-5 md:w-10 md:h-10 text-purple-400 group-hover:text-main-title duration-0" />
+                        </span>
+                    </Link>
+
+                    <MoveDown className="absolute -top-6 right-[2.9rem] text-indigo-500 md:w-10 md:h-10 md:-top-10 lg:w-14 lg:h-14 lg:right-[2.5rem] lg:-top-[3.7rem]" />
+                    <MoveLeft className="absolute text-indigo-500 -right-7 md:w-10 md:h-10 md:-right-12 lg:w-14 lg:h-14 lg:-right-16" />
+                    <MoveUp className="absolute top-7 right-[2.9rem] text-indigo-500 md:w-10 md:h-10 md:top-12 lg:w-14 lg:h-14 lg:right-[2.5rem] lg:top-[3.3rem]" />
+                </h5>
+            </div>
+        </div>
     )
 
 }
