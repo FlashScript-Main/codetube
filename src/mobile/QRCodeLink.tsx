71d3f9a1c6543743a9b64d37a71266549190c539
FlashScript <itzflashscript@gmail.com>
2024-07-23 17:54:32 +0330

    Fixed deployment bugs

--- src/mobile/QRCodeLink.tsx
+++ src/mobile/QRCodeLink.tsx
@@ -18,7 +18,7 @@ const QRCodeLink = () => {
             </DropdownMenuTrigger>
             
             <DropdownMenuContent className="flex py-4 px-3 justify-center items-center gap-4 bg-body-bg-light border border-red-600 dark:border-blue-400 shadow-md shadow-red-500/50 dark:shadow-blue-500/50 mb-4">
-                <QRCode value="CodeTube Link" />
+                <QRCode value="https://codetube-flashscript.netlify.app" />
             </DropdownMenuContent>
         </DropdownMenu>
     )
